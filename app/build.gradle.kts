0d266208a62608151cf1c638db67073a2cbc2c59
海獺 <brianchou452@gmail.com>
2024-05-19 19:52:16 +0800

    fix: 🐛嘗試修正應用程式啟動時 Crash 的問題 - 調降 accompanist 版本(https://github.com/google/accompanist/issues/1752)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "cc.seaotter.tomatoes"
         minSdk = 26
         targetSdk = 34
-        versionCode = 5
-        versionName = "0.2.2"
+        versionCode = 6
+        versionName = "0.2.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
